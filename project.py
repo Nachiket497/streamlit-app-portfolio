13b18c3fe8b87591f9cf84f10563964e7733a69d
Nachiket497 <1000nachiket@gmail.com>
2023-11-16 00:54:54 +0530

    repo cleaned

--- project.py
+++ project.py
@@ -7,12 +7,12 @@ from support_fun import project_dict
 
 num_columns = 4
 
-def make_card(img_path, title, tab, col):
+def make_card(img_path, title, code, tab, col):
     with tab :
         with col :
             ip = f"""
                   
-                <div class="card" onclick="window.open('./dum.html', '_blank')" style="width: 98%; height : 98%; border : 1px solid black; text-align:center; box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.2), 6px 6px 20px 0px rgba(0, 0, 0, 0.19);" >
+                <div class="card" onclick="window.open('{code}', '_blank')" style="width: 98%; height : 98%; border : 1px solid black; text-align:center; box-shadow: 4px 4px 8px 0px rgba(0, 0, 0, 0.2), 6px 6px 20px 0px rgba(0, 0, 0, 0.19);" >
                     <img src='data:image/png;base64,{img_to_bytes(img_path)}' alt="Avatar" style="width: 80%; padding: 5% 5% 5% 5%;  border-radius: 5px;">
                     <h4 id="demo" ><b>{title}</b></h4>
                     <div style="padding: 0% 0% 4% 0%;">
@@ -21,11 +21,9 @@ def make_card(img_path, title, tab, col):
                 </div>
                  
                 """
-            # if st.components.v1.html(ip, width=300, height=365):
-            #     print("clicked", title)
+            st.components.v1.html(ip, width=300, height=365)
 
-            with st.expander(title, expanded=False):
-                st.markdown(ip, unsafe_allow_html=True)
+            
       
 def make_project():
 
@@ -49,22 +47,22 @@ def make_project():
     for title in project_dict.keys():
         # try :
             if project_dict[title]['categaory'] == 'Robotics':
-                make_card(project_dict[title]['img'], title, t_robotics, cols[1][count[1]%num_columns] )
+                make_card(project_dict[title]['img'], title, project_dict[title]['code'], t_robotics,   cols[1][count[1]%num_columns] )
                 count[1] += 1
             
             elif project_dict[title]['categaory'] == 'Electronics/Control Systems':
-                make_card(project_dict[title]['img'], title,t_vlsi, cols[2][count[2]%num_columns]  )
+                make_card(project_dict[title]['img'], title, project_dict[title]['code'],   t_vlsi, cols[2][count[2]%num_columns]  )
                 count[2] += 1
             
             elif project_dict[title]['categaory'] == 'Software Development':
-                make_card(project_dict[title]['img'], title,t_software, cols[3][count[3]%num_columns]  )
+                make_card(project_dict[title]['img'], title, project_dict[title]['code'], t_software, cols[3][count[3]%num_columns]  )
                 count[3] += 1
 
             else:
                 print("Invalid category")
         
         
-            make_card(project_dict[title]['img'], title,t_all, cols[0][count[0]%num_columns] )
+            make_card(project_dict[title]['img'], title,project_dict[title]['code'], t_all, cols[0][count[0]%num_columns] )
             count[0] += 1
 
         # except Exception as e:
